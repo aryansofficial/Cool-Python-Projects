5c2b3274b27e10c5c279bdbe7a72b917cace81c7
aryansofficial <76882364+aryansofficial@users.noreply.github.com>
2021-02-15 18:26:39 +0530

    Add files via upload

---
+++ Pixel.py
@@ -0,0 +1,21 @@
+from pyautogui import position
+from pyautogui import screenshot
+from time import sleep
+from sys import stdout
+
+def print_no_newline(string):
+    stdout.write("\r")
+    stdout.write(string + '       ')
+    stdout.flush()
+
+try:
+    while True:
+        x,y = position() # This will get the x & y pos on screen
+        pixelColor = screenshot().getpixel((x,y)) # this will get the rgb val
+        string =  f'rgb{pixelColor}'
+        print_no_newline(string)
+        sleep(3)
+
+except KeyboardInterrupt:
+    print('')
+    print('Exiting :..')

d0fa6ec6d0205467fcc26600adff1e6a4ac30e02
aryansofficial <76882364+aryansofficial@users.noreply.github.com>
2021-06-24 02:03:14 +0530

    Update Pixel.py

--- Pixel.py
+++ Pixel.py
@@ -3,18 +3,36 @@ from pyautogui import position
 from pyautogui import screenshot
 from time import sleep
 from sys import stdout
+import blessed
 
-def print_no_newline(string):
-    stdout.write("\r")
-    stdout.write(string + '       ')
-    stdout.flush()
+
+
+color_list = []
+
+
+def print_block(color):
+    term = blessed.Terminal()
+    print(f'rgb{color}')
+    print(f'{term.on_color_rgb(color[0],color[1],color[2])}             {term.normal}')
+    print(f'{term.on_color_rgb(color[0],color[1],color[2])}             {term.normal}')
+    print(f'{term.on_color_rgb(color[0],color[1],color[2])}             {term.normal}')
+    print(f'{term.on_color_rgb(color[0],color[1],color[2])}             {term.normal}')
+    print(f'{term.on_color_rgb(color[0],color[1],color[2])}             {term.normal}')
+
+
+def print_no_newline(string):    
+    if string in color_list:
+        color_list.append(string)
+        return
+
+    color_list.append(string)
+    print_block(string)
 
 try:
     while True:
         x,y = position() # This will get the x & y pos on screen
         pixelColor = screenshot().getpixel((x,y)) # this will get the rgb val
-        string =  f'rgb{pixelColor}'
-        print_no_newline(string)
+        print_no_newline(pixelColor)
         sleep(3)
 
 except KeyboardInterrupt:
